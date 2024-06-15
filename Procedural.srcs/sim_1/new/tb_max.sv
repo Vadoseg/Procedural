26dd666aee90d6f6354f3670d739eab5e715087b
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-06-15 15:18:22 +0700

    Создал модуль max
    * Модуль считает максимальное число в пачке  данных
    * Создал тестбенч для этого модуля
    * Добавил описание модуля и TODO

---
+++ Procedural.srcs/sim_1/new/tb_max.sv
@@ -0,0 +1,103 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/05/2024 01:13:01 PM
+// Design Name: 
+// Module Name: tb_max
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_max#(
+        parameter int G_BYT = 1,
+        parameter int G_BIT_WIDTH = 8 * G_BYT
+    )(
+
+    );
+
+    localparam T_CLK = 1.0;
+    logic i_clk = '0;
+    logic i_rst = '0;
+    logic [G_BIT_WIDTH-1:0] i_data = '0;
+    logic i_valid = '0;
+    logic i_last  = '0;
+
+    task send_data_pkt;
+        begin
+            #(T_CLK*5);
+            i_valid <= '1;
+            for (int i = 1; i < 11; i++) begin
+                i_data <= i;
+                #(T_CLK/2.0);  
+            end
+            i_last <= '1;
+            i_data <= 24;
+            #(T_CLK/2.0);
+            i_valid <= '0;
+            i_data  <= '0;
+            i_last  <= '0;
+        end   
+    endtask : send_data_pkt
+        
+    
+    task send_max_check;
+        begin
+            #(T_CLK*5);
+            i_valid <= '1;
+            for (int i = 1; i < 11; i++) begin
+                i_data <= i;
+                #(T_CLK/2.0);  
+            end
+            
+            i_data <= 24;
+            #(T_CLK/2.0);
+            i_data <= 1;
+            #(T_CLK/2.0);
+            i_data <= 23;
+            #(T_CLK/2.0);
+            i_data <= 10;
+            #(T_CLK/2.0);
+            i_last <= '1;
+            i_data <= 5;
+            #(T_CLK/2.0);
+            i_valid <= '0;
+            i_data  <= '0;
+            i_last  <= '0;
+        end   
+    endtask : send_max_check
+
+
+    always#(T_CLK/2.0) i_clk <= ~i_clk;
+
+
+    initial begin
+        send_data_pkt;
+        #(T_CLK * 5);
+        send_max_check;
+    end
+
+    max#(
+        .G_BIT_WIDTH    (G_BIT_WIDTH)
+    ) MAX (
+        .i_clk      (i_clk),
+        .i_rst      (i_rst),
+        .i_data     (i_data),
+        .i_valid    (i_valid),
+        .i_last     (i_last),
+
+        .o_max_data (o_max_data),
+        .o_valid    (o_valid)
+    );
+endmodule
--- Procedural.srcs/sources_1/new/max.sv
+++ Procedural.srcs/sources_1/new/max.sv
@@ -1,26 +1,103 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: LEMZ-T
+// Engineer: Vadim V. Hvatov
 // 
 // Create Date: 06/05/2024 12:59:57 PM
-// Design Name: 
 // Module Name: max
-// Project Name: 
-// Target Devices: 
+// Project Name: Procedural
+// Target Devices: 7Series+
 // Tool Versions: 
-// Description: 
+// Description: Calculate Maximum data from datapack
 // 
-// Dependencies: 
+// Dependencies: None
 // 
 // Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
+// Revision 0.11 - Calculating maximum
+// Additional Comments: TODO reset flag
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module max(
+module max#(
+        parameter int G_BIT_WIDTH   = 8
+    )(
+        input logic                     i_clk,
+        input logic                     i_rst,
+        input logic [G_BIT_WIDTH-1:0]   i_data,
+        input logic                     i_valid,
+        input logic                     i_last,
 
+        output logic                    o_valid     = '0,
+        output logic [G_BIT_WIDTH-1:0]  o_max_data  = '0
     );
+
+    logic [G_BIT_WIDTH-1:0] w_buf_max       = '0;
+    logic                   w_max_check     = '0;
+    logic                   w_last          = '0;
+
+    logic                   q_valid         = '0;
+    logic [G_BIT_WIDTH-1:0] q_data          = '0;
+    logic                   q_last          = '0;
+
+    logic                   q_valid_2       = '0;
+    logic [G_BIT_WIDTH-1:0] q_data_2        = '0;
+    logic                   q_last_2        = '0;
+
+    always_ff @(i_clk) begin : maximum
+        
+        if (i_valid) begin
+            
+            q_valid     <= i_valid;
+            q_data      <= i_data;     
+
+            q_last      <= (i_last) ? 1 : 0;
+        end
+        
+        if (q_valid) begin          
+            
+            w_max_check     <= (w_buf_max < q_data) ? 1 : 0;
+            q_valid_2       <=  q_valid;
+            q_data_2        <=  q_data;
+
+            if (q_last) begin
+                q_data      <= '0;
+                q_valid     <= '0;
+                q_last      <= '0;
+
+                q_last_2    <= '1;
+            end
+        end
+
+        if (q_valid_2) begin
+            if (w_max_check) begin
+                w_buf_max   <= q_data_2;
+            end
+            
+
+            if (q_last_2) begin
+                q_data_2    <= '0;
+                q_valid_2   <= '0;
+                q_last_2    <= '0;
+
+                w_last      <= q_last_2;  
+            end
+            
+        end
+
+        if (w_last ) begin
+                o_max_data      <= w_buf_max;
+                o_valid         <= '1;
+                w_last          <= '0;
+                w_max_check     <= '0;
+            end
+
+        if (o_valid) begin
+            w_buf_max   <= '0;
+            o_valid     <= '0;
+            o_max_data  <= '0;
+        end
+    end
+
+
 endmodule
