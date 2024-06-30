c058002ac9540ed42e84003a90778c61f5b9573e
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-06-30 15:08:23 +0700

    Final mem

--- Procedural.srcs/sources_1/new/mem.sv
+++ Procedural.srcs/sources_1/new/mem.sv
@@ -20,52 +20,51 @@
 
 
 module mem#(
-        parameter int G_BIT_WIDTH   =  16,
-        parameter int G_INDX_WIDTH  =  8,
+        parameter int G_DATA_WIDTH  = 24,
         parameter int G_ADDR_WIDTH  =  5,
-        parameter int G_MEM_DEPTH   =  2**G_ADDR_WIDTH
+        
+        localparam int G_MEM_DEPTH   =  2**G_ADDR_WIDTH
     )(
         input   wire                        i_clk,
         input   wire                        i_wr_valid,     // Valid for input writing
-        input   wire   [ G_BIT_WIDTH-1:0]   i_wr_data,      // Input Writing data
-        input   wire   [G_INDX_WIDTH-1:0]   i_indx_data,    // WHAT TO DO
-
+        input   wire   [G_DATA_WIDTH-1:0]   i_wr_data,      // Input Writing data
         input   wire                        i_rd_valid,     // Valid for output reading
+        // input wire i_mem_ready
 
-        output  logic    [ G_BIT_WIDTH-1:0] [0:G_MEM_DEPTH-1]   o_rd_data = '0    // Output reading data
-    );                   // Maybe change types
+        output  bit                         o_ar_valid  = '0,
+        output  logic  [G_DATA_WIDTH-1:0]   o_rd_data   = '0    // Output reading data
+
+
+        /* // Master
+        input  reg  s_axil_awready,  output   wire s_axil_awvalid,  reg [G_ADDR_W - 1 : 0]  s_axil_awaddr,   logic  [2 : 0]             s_axil_awprot,          //  write addr
+        input  reg  s_axil_wready,   output   wire s_axil_wvalid,   reg [G_DATA_W - 1 : 0]  s_axil_wdata,    reg    [G_DATA_B - 1 : 0]  s_axil_wstrb,           //  write data 
+        output   wire s_axil_bready,   input  reg  s_axil_bvalid,   reg [1 : 0]             s_axil_bresp,                                                       //  write resp 
+        input  reg  s_axil_arready,  output   wire s_axil_arvalid,  reg [G_ADDR_W - 1 : 0]  s_axil_araddr,   logic  [2 : 0]             s_axil_arprot,          //  read addr 
+        output   wire s_axil_rready,  input  reg  s_axil_rvalid,   reg [G_DATA_W - 1 : 0]  s_axil_rdata,    reg    [1 : 0]             s_axil_rresp           //  read data & resp */
+
+    );
 
     reg     [G_ADDR_WIDTH-1:0]   q_wr_addr                  = '0;
+    reg     [G_ADDR_WIDTH-1:0]   q_wr_addr_prev             = '0;
     reg     [G_ADDR_WIDTH-1:0]   q_r_addr                   = '0; 
     
-    reg signed    [ G_BIT_WIDTH-1:0][0:G_MEM_DEPTH-1]    q_mem    = '0;
-
-    reg                          q_vld                      = '0;
-    reg     [G_INDX_WIDTH-1:0]   q_indx                     = '0;
+    reg     [G_DATA_WIDTH-1:0]   q_mem [0:G_MEM_DEPTH-1]    = '{default:G_DATA_WIDTH'(1'b0)};
 
     always_ff@(posedge i_clk) begin
         
         if(i_wr_valid) begin
-            q_wr_addr             <= q_wr_addr + 1;
-            q_mem [q_wr_addr]     <= i_wr_data;
-            // q_mem [q_wr_addr]     <= i_wr_data + i_indx_data;
-            // q_mem     <= i_indx_data;
-            q_mem [q_wr_addr] [0:$size(q_mem)-16] <= i_wr_data;
-            // q_mem /* [q_wr_addr]  */[16:$size(q_mem)-8] <= i_indx_data;
-            // q_vld                 <= i_wr_valid;
-            // q_indx                <= i_indx_data;
+            q_wr_addr           <= q_wr_addr + 1;
+            q_mem[q_wr_addr]    <= i_wr_data;
+            q_wr_addr_prev      <= q_wr_addr;
+            
         end
 
-        // if (q_vld) begin
-        //     q_mem[q_wr_addr]        <= q_indx;
-        //     q_wr_addr               <= q_wr_addr + 1;
-        // end
-
-        if(i_rd_valid /* && q_r_addr <= q_wr_addr */) begin
-            o_rd_data[q_r_addr]   <=  q_mem[q_r_addr];
+        if(i_rd_valid && q_r_addr < q_wr_addr_prev) begin
             q_r_addr    <=  q_r_addr + 1;
+            
         end
         
+        o_rd_data       <=  q_mem[q_r_addr];
+        o_ar_valid      <= '1;
     end
-
 endmodule
